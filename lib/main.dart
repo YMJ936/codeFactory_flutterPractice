5f42638df4345a043a9d3a53abbfa6b1c39e42ce
YMJ936 <aud780@gmail.com>
2024-02-14 23:37:45 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world/screen/home_screen.dart';
+
+void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
