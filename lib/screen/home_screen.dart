b081c2f395145dba0be87a79b3e5998519611c5d
YMJ936 <aud780@gmail.com>
2024-02-20 15:35:30 +0900

    Make U&I

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,60 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'dart:async';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
- class _HomeScreenState extends State<HomeScreen> {
-
-  final PageController pageController = PageController();
-
-  @override
-  void initState() {
-    super.initState();
-
-    Timer.periodic(Duration(seconds: 3),
-            (timer) {
-      int? nextPage = pageController.page?.toInt();
-
-      if (nextPage == null) {
-        return;
-      }
-
-      if (nextPage == 4) {
-        nextPage = 0;
-      } else {
-        nextPage++;
-      }
-      pageController.animateToPage(
-          nextPage,
-          duration: Duration(milliseconds: 500),
-          curve: Curves.ease,
-      );
-            },
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
-
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
-
   return Scaffold(
-  body: PageView(
-    controller: pageController,
-  children: [1, 2, 3, 4, 5]
-      .map(
-  (number) => Image.asset(
-  'asset/img/image_$number.jpeg',
-  fit: BoxFit.cover,
-  ),
-  )
-      .toList(),
-  ),
+    backgroundColor: Colors.pink[100],
+    body: SafeArea(
+      top: true,
+      bottom: false,
+      child: Column(
+        mainAxisAlignment:  MainAxisAlignment.spaceBetween,
+
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          _DDay(),
+          _CoupleImage(),
+        ],
+      ),
+    ),
   );
   }
+}
+
+class _DDay extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Column(
+      children: [
+        const SizedBox(height: 16.0),
+        Text(
+          'U&I',
+        ),
+        const SizedBox(height: 16.0),
+        Text(
+          '우리 처음 만난 날',
+        ),
+        Text(
+          '2021.11.23',
+        ),
+        const SizedBox(height: 16.0),
+        IconButton(
+          iconSize: 60.0,
+          onPressed: () {},
+            icon: Icon(
+              Icons.favorite,
+            ),
+        ),
+        const SizedBox(height: 16.0),
+        Text(
+          'D+365',
+        ),
+      ],
+    );
   }
+}
+
+class _CoupleImage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Center(
+      child: Image.asset(
+        'asset/img/middle_image.png',
+
+        height: MediaQuery.of(context).size.height / 2,
+      ),
+    );
+  }
+}
