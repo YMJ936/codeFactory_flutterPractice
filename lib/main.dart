59e8cd4b8a8a76620ee3380e9ad1695c7c900850
YMJ936 <aud780@gmail.com>
2024-03-01 23:45:18 +0900

    Make calendar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:hello_world/screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting();
+
   runApp(
     MaterialApp(
       home: HomeScreen(),
